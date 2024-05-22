3264ca95151b6f3f141f286c01a772a01ed9c2b2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-21 20:22:22 -0600

    add navbar ando footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <header>
+          <NavBar />
+        </header>
+        <main className={inter.className}>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
